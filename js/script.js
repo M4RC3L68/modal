4a6feddb92d5759e1525ab33e0b45bb4499eb0bf
marcel <marcelinocost69@gmail.com>
2023-04-26 10:09:25 -0300

    commit - 02

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
-const modal = document.querySelector("#open-modal")
+const openModalButton = document.querySelector("#open-modal")
+const closeModalButton = document.querySelector("#close-modal")
+const modal = document.querySelector("#modal")
+const fade = document.querySelector("#fade")
 
+
+
+const janela = [openModalButton, closeModalButton, fade]
+janela.forEach((el) => {
+  el.addEventListener("click", () => console.log("teste"))
+});
+
+   
